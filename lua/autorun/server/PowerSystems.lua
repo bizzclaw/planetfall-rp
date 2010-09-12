da9d797f2369e4b8e0d837034d126f9bcb30f188
kaffse <kaffse@44b2f168-e011-11de-ab17-5d3be3536607>
2010-09-12 20:03:37 +0000

    Hell if I know what is different but hey, its newer

--- lua/autorun/server/PowerSystems.lua
+++ lua/autorun/server/PowerSystems.lua
@@ -3,8 +3,8 @@
  ---------------------------*/
 
 cur_power = 10000 //starting power/current power
-item = { ["power"] = 1, ["gen"] = 1, ["lift"] = 0, ["shield"] = 0 }
-shield_parts = { [1] = "Sld1_B1", [2] = "Sld1_B2", [3] = "Sld1_B3", [4] = "Sld1_B4", [5] = "Sld1_B5", [6] = "Sld1_B6", [7] = "Sld1_B7", [8] = "Sld1_B8" }
+item = { ["power"] = 1, ["gen"] = 1, ["lift"] = 0, ["shield"] = 1 }
+//shield_parts = { [1] = "Sld1_B1", [2] = "Sld1_B2", [3] = "Sld1_B3", [4] = "Sld1_B4", [5] = "Sld1_B5", [6] = "Sld1_B6", [7] = "Sld1_B7", [8] = "Sld1_B8" }
 lockdown_doors = { [1] = "Brdg_D", [2] = "LQ_D2", [3] = "Holo_D", [4] = "LQ_D", [5] = "HSC_D2", [6] = "Utl_BD", [7] = "Civ_BD", [8] = "Med_D", [9] = "Cic_BD2", [10] = "H2_D2", [11] = "AS2_D2", [12] = "H2_D3", [13] = "Prn_D", [14] = "H1_D4", [15] = "H2_D1", [16] = "BS_D", [17] = "HSC_D", [18] = "Gen_D", [19] = "H1_D", [20] = "H1_D1" }
 lift_power = 100 //the ammount of power the lift takes per clock
 shield_power = 900 //read above, guess
@@ -32,15 +32,19 @@ function Clock()
 	if item["power"] == 1 && item["shield"] == 1 then cur_power = cur_power - shield_power end
 	
 	if item[ "shield" ] == 0 && s_enabled == 1 then 
-			for i,v in pairs ( ents.FindByName( "Sld1_B*" ) ) do
-				v:Fire ( "Disable", "" , ((i / 10) - 0.1 ) )
+			local cur_ents = ents.FindByName( "Sld1_B*" )
+			table.sort ( cur_ents )
+			for i,v in pairs ( cur_ents ) do
+				v:Fire ( "Disable", "" , ( ( i / 10 ) - 0.1 ) )
 			end
 		 s_enabled = 0
 	end
 
 	if item[ "shield" ] == 1 && s_enabled == 0 then 
-			for i,v in pairs ( ents.FindByName( "Sld1_B*" ) ) do
-				v:Fire ( "Enable", "" , (5 + ( (i / 10) - 0.1) ) )
+			local cur_ents = ents.FindByName( "Sld1_B*" )
+			table.sort ( cur_ents )
+			for i,v in pairs ( cur_ents ) do
+				v:Fire ( "Enable", "" , ( 5 + ( (i / 10) - 0.1) ) )
 			end
 		s_enabled = 1
 	end
@@ -71,7 +75,7 @@ function Off(i)
 end
 
 /*----------------------------------------------------------------------------------------------------
-Pretty simple fucntion that Shadow asked for that will lockdown the ship for him without Hammer Logic|
+Pretty simple fucntion that Shadow asked for that will lockdown the ship for him without Hammer Logic, funny point actually, this makes the ship impossible to get though :P |
  ---------------------------------------------------------------------------------------------------*/
 
 function Lockdown()
@@ -84,10 +88,10 @@ function Lockdown()
 	end
 
 	for k,v in pairs(lockdown_doors) do
-		for j,i in pairs(ents.FindByName(v)) do
-			i:Fire (input_one, "", 0)
+		for j,i in pairs( ents.FindByName(v) ) do
+			i:Fire ( input_one , "" , 0 )
 			if input_one == "Close" then
-				i:Fire (input_two, "", 0)
+				i:Fire ( input_two , "" , 0 )
 			end
 		end
 	end
@@ -98,28 +102,11 @@ end
  -----------------------------------------------------------------*/
 
 function Debug() 
-	for _,v in pairs(player.GetAll()) do
-		v:PrintMessage (HUD_PRINTTALK, "Current Power is " .. cur_power)
-		v:PrintMessage (HUD_PRINTTALK, "Gen is in position " .. item["gen"])
-		v:PrintMessage (HUD_PRINTTALK, "Power is in position " .. item["power"])
-		v:PrintMessage (HUD_PRINTTALK, "Lift is in position " .. item["lift"])
-		v:PrintMessage (HUD_PRINTTALK, "Shield is in position " .. item["shield"])
+	for _,v in pairs( player.GetAll() ) do
+		v:PrintMessage ( HUD_PRINTTALK , "Current Power is " .. cur_power )
+		v:PrintMessage ( HUD_PRINTTALK , "Gen is in position " .. item["gen"] )
+		v:PrintMessage ( HUD_PRINTTALK , "Power is in position " .. item["power"] )
+		v:PrintMessage ( HUD_PRINTTALK , "Lift is in position " .. item["lift"] )
+		v:PrintMessage ( HUD_PRINTTALK , "Shield is in position " .. item["shield"] )
 	end
 end 
-	//Dev section, used to test everything and make sure it worked!
-if CLIENT then
-	concommand.Add ("Clock", Clock)
-	concommand.Add ("GenSwitch", function() Switch("gen") end)
-	concommand.Add ("PowerSwitch", function() Switch("power") end)
-	concommand.Add ("LiftSwitch", function() Switch("lift") end)
-	concommand.Add ("ShieldSwitch", function() Switch("shield") end)
-	concommand.Add ("Test", Debug)
-	concommand.Add ("GenOn", function() On("gen") end)
-	concommand.Add ("GenOff", function() Off("gen") end)
-	concommand.Add ("PowerOn", function() On("power") end)
-	concommand.Add ("PowerOff", function() Off("power") end)
-	concommand.Add ("LiftOn", function() On("lift") end)
-	concommand.Add ("LiftOff", function() Off("lift") end)
-	concommand.Add ("ShieldOn", function() On("shield") end)
-	concommand.Add ("ShieldOff", function() Off("shield") end)
-end
